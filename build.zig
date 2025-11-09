d2f6e50949fa454f71a041a20b70c595368c9469
kris <kris@wecitylnl>
2025-11-09 18:01:36 +0100

    more zig idiomatic

--- build.zig
+++ build.zig
@@ -4,10 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("glfw_vulkan_template", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
     const vulkan = b.dependency("vulkan_zig", .{
         .registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml"),
     }).module("vulkan-zig");
@@ -30,9 +26,6 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
-            .imports = &.{
-                .{ .name = "glfw_vulkan_template", .module = mod },
-            },
         }),
     });
 
@@ -59,12 +52,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
@@ -72,6 +59,5 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,48 +1,56 @@
 const std = @import("std");
-const glfw_vulkan_template = @import("glfw_vulkan_template");
 const glfw = @import("zglfw");
 const vk = @import("vulkan");
 
 const WIDTH: u16 = 800;
 const HEIGHT: u16 = 600;
-var window: *glfw.Window = undefined;
 
 pub fn main() u8 {
-    run() catch return 1;
+    var app = TriangleApplication.init();
+    defer app.cleanup();
+    try app.run() catch return 1;
 
     return 0;
 }
 
-fn run() !void {
-    try initWindow();
-    initVulkan();
-    mainLoop();
-    cleanup();
-}
+const TriangleApplication = struct {
+    const Self = @This();
+    window: ?glfw.Window = null,
 
-fn initWindow() !void {
-    try glfw.init();
-
-    std.debug.print("Initialized GLFW\n", .{});
-
-    glfw.windowHint(glfw.WindowHint.client_api, glfw.ClientApi.no_api);
-    glfw.windowHint(glfw.WindowHint.resizable, false);
-    window = try glfw.createWindow(WIDTH, HEIGHT, "Vulkan", null);
-    defer glfw.destroyWindow(window);
-
-    while (!window.shouldClose(window)) {
-        glfw.pollEvents();
-
-        // render your things here
-
-        window.swapBuffers();
+    pub fn init() Self {
+        return Self{};
     }
-}
-fn initVulkan() void {}
 
-fn mainLoop() void {}
+    pub fn run(self: *Self) !void {
+        try self.initWindow();
+        self.initVulkan();
+        self.mainLoop();
+    }
 
-fn cleanup() void {
-    glfw.destroyWindow(window);
-    glfw.terminate();
-}
+    fn initWindow(self: *Self) !void {
+        try glfw.init(.{});
+
+        std.debug.print("Initialized GLFW\n", .{});
+
+        self.window = try glfw.createWindow(WIDTH, HEIGHT, "Vulkan", null, null, .{
+            .client_api = .no_api,
+            .resizable = false,
+        });
+
+        while (!self.window.shouldClose(self.window)) {
+            glfw.pollEvents();
+
+            // render your things here
+
+            self.window.swapBuffers();
+        }
+    }
+    fn initVulkan(_: *Self) void {}
+
+    fn mainLoop(_: *Self) void {}
+
+    fn cleanup(self: *Self) void {
+        if (self.window != null) self.window.?.destroy();
+        glfw.terminate();
+    }
+};
