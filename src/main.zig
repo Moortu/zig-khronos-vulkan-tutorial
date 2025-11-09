3bb8c64fa8ba1525a1c47f1fb7e559314c22c761
kris <kris@wecitylnl>
2025-11-09 18:48:27 +0100

    more idiomatic zig with zglfw

--- src/main.zig
+++ src/main.zig
@@ -2,20 +2,20 @@ const std = @import("std");
 const glfw = @import("zglfw");
 const vk = @import("vulkan");
 
-const WIDTH: u16 = 800;
-const HEIGHT: u16 = 600;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
 pub fn main() u8 {
     var app = TriangleApplication.init();
     defer app.cleanup();
-    try app.run() catch return 1;
+    app.run() catch return 1;
 
     return 0;
 }
 
 const TriangleApplication = struct {
     const Self = @This();
-    window: ?glfw.Window = null,
+    window: ?*glfw.Window = null,
 
     pub fn init() Self {
         return Self{};
@@ -28,21 +28,20 @@ const TriangleApplication = struct {
     }
 
     fn initWindow(self: *Self) !void {
-        try glfw.init(.{});
+        try glfw.init();
 
         std.debug.print("Initialized GLFW\n", .{});
 
-        self.window = try glfw.createWindow(WIDTH, HEIGHT, "Vulkan", null, null, .{
-            .client_api = .no_api,
-            .resizable = false,
-        });
+        glfw.windowHint(glfw.WindowHint.client_api, glfw.ClientApi.no_api);
+        glfw.windowHint(glfw.WindowHint.resizable, false);
+        self.window = try glfw.Window.create(WIDTH, HEIGHT, "Vulkan", null);
 
-        while (!self.window.shouldClose(self.window)) {
+        while (!self.window.?.shouldClose()) {
             glfw.pollEvents();
 
             // render your things here
 
-            self.window.swapBuffers();
+            self.window.?.swapBuffers();
         }
     }
     fn initVulkan(_: *Self) void {}
@@ -50,7 +49,10 @@ const TriangleApplication = struct {
     fn mainLoop(_: *Self) void {}
 
     fn cleanup(self: *Self) void {
-        if (self.window != null) self.window.?.destroy();
+        if (self.window) |win| {
+            win.destroy();
+            self.window = null;
+        }
         glfw.terminate();
     }
 };
